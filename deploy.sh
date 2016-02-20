87554c9a03032be850e0d6048000f5d74b68f2ab
Gavin Noronha <gavinln@hotmail.com>
2016-02-20 14:13:28 -0800

    rebuilding site Sat Feb 20 14:13:28 PST 2016

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:gavinln/blog-site.git gh-pages]]"
+git subtree push --prefix=public git@github.com:gavinln/blog-site.git gh-pages
+
